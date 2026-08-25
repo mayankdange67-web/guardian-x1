4ef68f66705865c508dce65ffd697182ad566d71
mayankdange67-web <mayankdange67@gmail.com>
2026-08-25 22:14:38 +0530

    Rename speech_to_text.py.py to speech_to_text.py

---
+++ guardian-x1/src/voice.ai/speech_to_text.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+class LocalSTTEngine:
+    def transcribe_audio(self, audio_path): return "takeoff to 5 meters"
--- guardian-x1/src/voice.ai/speech_to_text.py.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python3
-class LocalSTTEngine:
-    def transcribe_audio(self, audio_path): return "takeoff to 5 meters"
