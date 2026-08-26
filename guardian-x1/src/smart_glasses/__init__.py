3b2eb79990ba995d362f4b6945dc7d5a65789d5e
mayankdange67-web <mayankdange67@gmail.com>
2026-08-26 11:46:43 +0530

    Update __init__.py

--- guardian-x1/src/smart_glasses/__init__.py
+++ guardian-x1/src/smart_glasses/__init__.py
@@ -1,7 +1,9 @@
 """
-Guardian X-1 AR Smart Glasses Node Package.
+Guardian X-1 Smart Glasses Subsystem Package.
+Handles Heads-Up Display (HUD) telemetry streaming and optical notifications.
 """
 
-from .glasses_node import SmartGlassesNode
+from .glasses_node import GlassesNode
 
-__all__ = ["SmartGlassesNode"]
+__all__ = ["GlassesNode"]
+__version__ = "1.2.0"
